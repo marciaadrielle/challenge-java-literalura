38144cd641428e86c9507b855c9a267a7a6084ac
marcia <marcia.adrielle@gmail.com>
2025-12-21 08:04:20 -0300

    Implementa busca de livros na Gutendex

--- src/main/java/br/com/alura/literalura/principal/Principal.java
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -1,9 +1,13 @@
 package br.com.alura.literalura.principal;
 
+import br.com.alura.literalura.model.DadosLivro;
+import br.com.alura.literalura.model.GutendexResponse;
 import br.com.alura.literalura.service.ConsumoApi;
 import br.com.alura.literalura.service.ConverteDados;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 @Component
@@ -13,6 +17,9 @@ public class Principal {
     private Scanner leitura = new Scanner(System.in);
 
     private final String ENDERECO = "https://gutendex.com/books?search=";
+
+
+    private List<DadosLivro> livrosBuscados = new ArrayList<>();
 
     public void exibeMenu() {
         var opcao = -1;
@@ -62,6 +69,18 @@ public class Principal {
     }
 
     private void buscarLivroPorTitulo() {
+        System.out.println("Digite o nome do livro que deseja buscar: ");
+        var nomeLivro = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeLivro.replace(" ","%20").toLowerCase());
+        var resposta = conversor.obterDados(json, GutendexResponse.class);
+
+        if (resposta.results() != null && !resposta.results().isEmpty()){
+        DadosLivro dados = resposta.results().get(0);
+        System.out.println(dados);
+        } else {
+            System.out.println("Livro não encontrado");
+        }
+
 
     }
 
